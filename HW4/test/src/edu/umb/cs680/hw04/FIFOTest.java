c619c682bd107f45fd07a3dbeb31b7cec8c9e854
Gaddam Hemanth <40312851+ghemanth15@users.noreply.github.com>
2022-03-20 22:30:54 -0400

    Uploaded HW4

---
+++ HW4/test/src/edu/umb/cs680/hw04/FIFOTest.java
@@ -0,0 +1,52 @@
+package edu.umb.cs680.hw04;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import edu.umb.cs680.hw04.FIFO;
+
+class FIFOTest {
+	private static FIFO cache;
+	private static Path path1;
+	private static Path path2;
+	private static Path path3;
+	private static Path path4;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		cache = new FIFO(3);
+		path1 = Paths.get("filesfortesting/file1.txt");
+		path2 = Paths.get("filesfortesting/file2.txt");
+		path3 = Paths.get("filesfortesting/file3.txt");
+		path4 = Paths.get("filesfortesting/file4.txt");
+		cache.fetch(path1);
+		cache.fetch(path2);
+		cache.fetch(path3);
+	}
+
+	@Test
+	public void fetchwithpath1() throws IOException {
+		assertEquals(Files.readString(path1), cache.fetch(path1));
+	}
+	
+	@Test
+	public void fetchwithpath2() throws IOException {
+		assertEquals(Files.readString(path2), cache.fetch(path2));
+	}
+	
+	@Test
+	public void fetchwithpath3() throws IOException {
+		assertEquals(Files.readString(path3), cache.fetch(path3));
+	}
+	
+	@Test
+	public void fetchwithpath4() throws IOException {
+		assertEquals(Files.readString(path4), cache.fetch(path4));
+		
+	}
+
+}
