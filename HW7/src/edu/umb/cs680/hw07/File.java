9a7c737ca0a1cf1a425fda3ad90fd9c127246dc9
Gaddam Hemanth <40312851+ghemanth15@users.noreply.github.com>
2022-05-20 23:44:55 -0400

    Uploaded HW7

---
+++ HW7/src/edu/umb/cs680/hw07/File.java
@@ -0,0 +1,23 @@
+package edu.umb.cs680.hw07;
+
+import java.time.LocalDateTime;
+
+public class File extends FSElement{
+
+	public File(Directory parent, String name, int size, LocalDateTime creationTime) {
+		super(parent, name, size, creationTime);
+	}
+
+	public boolean isLink() {
+		return false;
+	}
+	
+	public boolean isDirectory() {
+		return false;
+	}
+
+	public boolean isFile() {
+		return true;
+	}
+
+}
